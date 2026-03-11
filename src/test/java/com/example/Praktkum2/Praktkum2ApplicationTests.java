1e8ca4872bbf38c1feb6697f20387f9571f27174
dzakyputra45 <dzakypratama45@gmail.com>
2026-03-11 11:15:54 +0700

    Initial commit

---
+++ src/test/java/com/example/Praktkum2/Praktkum2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Praktkum2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktkum2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
