491f83ecce98404c32a1abaeeded21476890dc37
dzakyputra45 <dzakypratama45@gmail.com>
2026-03-11 11:22:51 +0700

    Add Running app

---
+++ src/main/java/com/example/Praktkum2/Praktkum2Application.java
@@ -0,0 +1,13 @@
+package com.example.Praktkum2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktkum2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktkum2Application.class, args);
+	}
+
+}
