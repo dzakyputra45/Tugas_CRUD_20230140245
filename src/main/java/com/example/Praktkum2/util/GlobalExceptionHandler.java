0b5c35c5c0418f26d709877f9dced3798318ee14
dzakyputra45 <dzakypratama45@gmail.com>
2026-03-11 11:22:05 +0700

    add error handling

---
+++ src/main/java/com/example/Praktkum2/util/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.Praktkum2.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleRuntimeException(RuntimeException ex){
+
+        Map<String, String> error = new HashMap<>();
+
+        error.put("error", ex.getMessage());
+
+        return error;
+    }
+
+}
